f81ca403ad6313820012182c66a6b0ad8eb2c73e
Nazanin Taghavi <n.taghavi168@gmail.com>
2024-12-10 17:45:45 +0330

    fixed theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
@@ -12,6 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
+        preset: Aura,
         options: {
           prefix: 'p',
           darkModeSelector: 'system',
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { WebsiteComponent } from './website/website.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WebsiteComponent },
+  {
+    path: 'admin',
+    component: AdminPanelComponent,
+  },
+];
